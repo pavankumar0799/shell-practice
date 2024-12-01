f4f4f3fe4e18656ceb0603c3ed8672ad31bbe3fd
pavankumar <rpavankumar7382@gmail.com>
2024-12-01 19:17:10 +0530

    condition

---
+++ condition.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+number=10
+
+if [ $number -ge 20 ]
+   then
+     echo "number is greater than 20"
+
+    else
+      echo "numer is not greater than 20'
+
+fi
--- datatypes.sh
+++ datatypes.sh
@@ -3,6 +3,6 @@
 number1=10
 number2=20
 
-sum=$(number1+number2)
+sum=$((number1+number2))
 
 echo "sum of two number is:$sum"

69e689849e28fd90a1705be941bfd05839dc105d
pavankumar <rpavankumar7382@gmail.com>
2024-12-01 19:23:03 +0530

    condition

--- condition.sh
+++ condition.sh
@@ -4,9 +4,9 @@ number=10
 
 if [ $number -ge 20 ]
 then
-     echo "$number is greater than 20"
+    echo "$number is greater than 20"
 
 else
-      echo "$number is not greater than 20'
+    echo "$number is not greater than 20"
 
 fi

f72058d56f76444cc6b1ca2175bdd67a5a95edc2
pavankumar <rpavankumar7382@gmail.com>
2024-12-01 23:06:59 +0530

    install

---
+++ install git.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [  $userid -ne 0 ]
+then 
+  echo "take root user access"
+  exit 1
+else
+  echo "don't take root access"
+fi
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+  echo "git not installed"
+else 
+  echo "git installed"
+fi

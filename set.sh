055da96714971c1b34796e264b4553b21e08b525
pavankumar <rpavankumar7382@gmail.com>
2024-12-02 19:45:03 +0530

    loops

--- set.sh
+++ set.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 set -e
+#set -ex --> showing more information
 
+faiure(){
+    echo "failed at $1:$2"
+}
+
+trap 'failure "${line number}" "${bash command}"' err
 echo "this is success"
 echooo "this is failure"
 echo "this is success"
